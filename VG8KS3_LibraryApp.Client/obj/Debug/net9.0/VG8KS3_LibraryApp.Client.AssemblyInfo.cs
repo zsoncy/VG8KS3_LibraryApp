f9326d38aed966a9c79d5d34caaf3d62150c5f69
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-05-25 01:12:18 +0200

    feat(client): Add "Add Book" functionality on Books page with backend HTTP integration

--- VG8KS3_LibraryApp.Client/obj/Debug/net9.0/VG8KS3_LibraryApp.Client.AssemblyInfo.cs
+++ VG8KS3_LibraryApp.Client/obj/Debug/net9.0/VG8KS3_LibraryApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VG8KS3_LibraryApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3972cc982fef185cd5bde44bc6610d6c891efb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eefac6808a27fb11f5967ffd5d435a715e3fede")]
 [assembly: System.Reflection.AssemblyProductAttribute("VG8KS3_LibraryApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VG8KS3_LibraryApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

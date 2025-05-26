08c7819994574ba56f0bf40f710bcf54bc8a6c72
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-05-26 17:54:12 +0200

    feat(client): Add validation feedback to all Borrow pages on the frontend

--- VG8KS3_LibraryApp.Client/obj/Debug/net9.0/VG8KS3_LibraryApp.Client.AssemblyInfo.cs
+++ VG8KS3_LibraryApp.Client/obj/Debug/net9.0/VG8KS3_LibraryApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VG8KS3_LibraryApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d0840767ecbc5c5d4951c1431874d122f069f46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13b135809d13c411562f15a5b8268eac694c4be")]
 [assembly: System.Reflection.AssemblyProductAttribute("VG8KS3_LibraryApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VG8KS3_LibraryApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
